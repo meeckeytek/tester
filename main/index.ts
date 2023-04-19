0e638dc0226dac508d1dfa1ef5276af84eb3d2c5
meeckety tek <meeckeytek@gmail.com>
2023-04-19 14:52:28 +0000

    update

---
+++ main/index.ts
@@ -0,0 +1,10 @@
+import { Context, HttpRequest } from '@azure/functions';
+import { AzureHttpAdapter } from '@nestjs/azure-func-http';
+import { createApp } from '../src/main.azure';
+
+export default function(context: Context, req: HttpRequest): void {
+  AzureHttpAdapter.handle(createApp, context, req);
+  context.res.json({
+    text: "WELCOME MAN"
+});
+}
